2beaee7a40990c4bcdcd9fc3549c4d35b0e1903c
RKRAKSHITA <80333640+RKRAKSHITA@users.noreply.github.com>
2021-04-27 20:24:13 +0530

    Update project_main.c

--- project_main.c
+++ project_main.c
@@ -1,6 +1,6 @@
 /**
  * @file project_main.c
- * @author Bharath.G ()
+ * @author Rakshita.R.K ()
  * @brief Project to Blink an LED at 1000ms ON and 500 ms OFF Interval
  * @version 0.1
  * @date 2021-04-23
@@ -11,7 +11,7 @@
 #include "project_config.h"
 
 #include "user_utils.h"
-#include "blinky.h"
+#include "blink.h"
 
 /**
  * @brief Initialize all the Peripherals and pin configurations
@@ -20,7 +20,11 @@
 void peripheral_init(void)
 {
 	/* Configure LED Pin */
-	DDRB |= (1 << DDB0);
+	DDRB |= (1 << DDB0); //SET B0=1
+	DDRD &= ~(1<<PD0);
+	DDRD &= ~(1<<PD1);
+	PORTD |= (1<<PD0); //SET D0
+	PORTD |= (1<<PD1);
 }
 
 void change_led_state(uint8_t state)
@@ -43,11 +47,16 @@ int main(void)
 
 	for(;;)
 	{
-        change_led_state(LED_ON);
-		delay_ms(LED_ON_TIME);
-		
-        change_led_state(LED_OFF);
-		delay_ms(LED_OFF_TIME);	
+        if((!(PIND&(1<<PD0)))&&((!(PIND&(1<<PD1)))))
+		{
+			change_led_state(LED_ON);
+			delay_ms(LED_ON_TIME);
+		}
+		else
+		{
+        	change_led_state(LED_OFF);
+			delay_ms(LED_OFF_TIME);	
+		}
 	}
 	return 0;
 }
