ed80bb47afa21b1e16a4e864ed63b62f63bea589
RKRAKSHITA <80333640+RKRAKSHITA@users.noreply.github.com>
2021-04-27 22:34:02 +0530

    Update project_config.h

--- inc/project_config.h
+++ inc/project_config.h
@@ -2,7 +2,7 @@
 #define __PROJECT_CONFIG_H__
 /**
  * @file project_config.h
- * @author Bharath.R.K ()
+ * @author Rakshita.R.K ()
  * @brief Configuration file to define pins and ports for the interfaced peripherals
  * @version 0.1
  * @date 2021-04-23
