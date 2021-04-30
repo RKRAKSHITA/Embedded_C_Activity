1e0874a9abc007ec6bfd40daa6fe283c406a78e5
RKRAKSHITA <80333640+RKRAKSHITA@users.noreply.github.com>
2021-04-30 10:03:14 +0530

    Update USART.c

--- src/USART.c
+++ src/USART.c
@@ -1,34 +1,41 @@
-#define F_CPU 16000000UL
+/**
+ *@file uart.c
+ *@author Rakshita.R.K()
+ *@brief 
+ *@version 0.1
+ *@date 2021-04-29
+ *
+ *@copyright Copyright (c) 2021
+ *
+ */
 #include <avr/io.h>
-#include <util/delay.h>
-#define BAUD 9600
-#define BAUDRATE ((F_CPU)/(BAUD*16UL)-1)  ///<Baudrate calculation
+#include "USART.h"
 
-void USARTInit(void)
+/**
+ *@brief Initialise USART to print temperature values on monitor
+ *
+ *@param ubrr_val baud rate
+ */
+void USARTInit(uint16_t ubrr_val)
 {
-    ///Set baudrate
-    UBRR0H |= (BAUDRATE >> 8);
-    UBRR0L |= BAUDRATE;
+    //SET BAUD RATE
+    UBRR0L=ubrr_val;
+    UBRR0H=(ubrr_val>>8)&0x00ff;
+    UCSR0C=(1<<UMSEL00)|(1<<UCSZ01)|(1<<UCSZ00);
+    UCSR0B=(1<<RXEN0)|(1<<TXEN0)|(1<<RXCIE0)|(1<<TXCIE0);
 
-    ///To set character size as 8 bits
-    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
-
-    ///Enable transmit and receive interrupts
-    UCSR0B = (1 << RXEN0) | (1 << TXEN0) | (1 << RXCIE0) | (1 << TXCIE0);
 }
 
-char USARTReadChar(void)
+/**
+ *@brief To write the data to UDR and print in serial monitor
+ *
+ *@param data 
+ */
+void USARTWriteData(uint8_t data)
 {
-    ///Wait to receive data
-    while(!(UCSR0A & (1 << RXC0)));
+    while(!(UCSR0A & (1<<UDRE0)))
+    {
 
-    return UDR0;
-}
-
-void USARTWriteChar(char data)
-{
-    ///Wait to transmitt data
-    while(!(UCSR0A & (1 << UDRE0)));
-
-    UDR0 = data;
+    }
+    UDR0=data;
 }
