9c35291f15e55618bad69ab2581b0a8b9cc3ea8a
RKRAKSHITA <80333640+RKRAKSHITA@users.noreply.github.com>
2021-04-30 10:26:52 +0530

    Update project_main.c

--- project_main.c
+++ project_main.c
@@ -1,62 +1,49 @@
 /**
- * @file project_main.c
- * @author Rakshita.R.K ()
- * @brief Project to Blink an LED at 1000ms ON and 500 ms OFF Interval
- * @version 0.1
- * @date 2021-04-23
- * 
- * @copyright Copyright (c) 2021
- * 
+ *@file main.c
+ *@author Vichkshana (jsvichkshana@gmail.com)
+ *@brief Main application for seat heater project
+ *@version 0.1
+ *@date 2021-04-29
+ *
+ *@copyright Copyright (c) 2021
+ *
  */
-#include "project_config.h"
 
-#include "user_utils.h"
-#include "blink.h"
+#include <avr/io.h>
+#include <util/delay.h>
+#include "LED.h"
+#include "ADC.h"
+#include "PWM.h"
+#include "USART.h"
 
 /**
- * @brief Initialize all the Peripherals and pin configurations
- * 
- */
-void peripheral_init(void)
-{
-	/* Configure LED Pin */
-	DDRB |= (1 << DDB0); //SET B0=1
-	DDRD &= ~(1<<PD0);
-	DDRD &= ~(1<<PD1);
-	PORTD |= (1<<PD0); //SET D0
-	PORTD |= (1<<PD1);
-}
-
-void change_led_state(uint8_t state)
-{
-	LED_PORT = (state << LED_PIN);
-}
-
-
-/**
- * @brief Main function where the code execution starts
- * 
- * @return int Return 0 if the program completes successfully
- * @note PORTB0 is in sink config. i.e when pin is Low, the LED will turn OFF
- * @note PORTB0 is in sink config. i.e when pin is High, the LED will turn ON
+ *@brief Main funtion to adjust temperature of seat as per requirements
+ *
+ *@return int 
  */
 int main(void)
 {
-	/* Initialize Peripherals */
-	peripheral_init();
-
-	for(;;)
-	{
+    peripheral_init();
+    InitADC();
+    InitPwm();
+    USARTInit(103);
+    uint16_t temp;
+    while(1)
+    {
         if((!(PIND&(1<<PD0)))&&((!(PIND&(1<<PD1)))))
-		{
-			change_led_state(LED_ON);
-			delay_ms(LED_ON_TIME);
-		}
-		else
-		{
-        	change_led_state(LED_OFF);
-			delay_ms(LED_OFF_TIME);	
-		}
-	}
-	return 0;
+        {
+            change_led_state(LED_ON);
+            temp=ReadADC(0);
+            _delay_ms(200);
+            out_pwm(temp);
+        }
+        else
+        {
+          change_led_state(LED_OFF);
+          _delay_ms(LED_OFF_TIME);
+        }
+
+    }
+
+    return 0;
 }
